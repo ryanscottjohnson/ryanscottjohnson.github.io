a01c3d6381ada1d5935ccae367e4cb6e86d22b02
ryanscottjohnson <teacherman1@gmail.com>
2018-08-14 16:22:47 -0700

    building home and project pages

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-// import reducers from '../reducers';
-// import middleWare from '../middleware';
+import reducers from '../reducers';
+import middleWare from '../middleware';
 import Home from './home.jsx';
 
 const store = createStore(
     reducers,
-    applyMiddleware()
+    applyMiddleware(middlewares.logger)
 );
 
 class App extends React.Component {
