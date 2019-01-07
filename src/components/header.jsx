1dbe0663a78ca4965cd7e63a74636f14a845572d
ryanscottjohnson <teacherman1@gmail.com>
2019-01-07 13:24:14 -0800

    adjusting files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,31 +9,31 @@ class Header extends React.Component {
   render() {
     return <header>
         <nav class="header-nav">
-          <img src="/src/images/amber-kim-logo.png" alt="Amber Kim Logo" id="logo"/>
+          <img src="/src/images/" alt="Ryan Logo" id="logo"/>
           <div class="nav-links">
             <a id="about-link" href="#about-section">
               <FontAwesome name="user" size="2x" />
             </a>
-            <a href="mailto:amber@amberkimdesign.com">
+            <a href="mailto:ryanscottjohnson02@gmail.com">
               <FontAwesome name="envelope" size="2x" />
             </a>
-            <a href="https://www.linkedin.com/in/ambergkim/">
+            <a href="https://www.https://www.linkedin.com/in/ryanscottjohnson/">
               <FontAwesome name="users" size="2x" />
             </a>
-            <a href="https://github.com/ambergkim">
+            <a href="https://github.com/ryanscottjohnson">
               <FontAwesome name="code" size="2x" />
             </a>
-            <a href="https://docs.google.com/document/d/18TJfPpLyXWLke-iZx0MA0V2IJn9cA8BvoQGr4VBfiu0/edit?usp=sharing">
+            <a href="https://docs.https://docs.google.com/document/d/11DKYTJn8-BUbFcD4ykTigwKY723eMeYvD6g28rHXRmM/edit?usp=sharing.com/document/d/18TJfPpLyXWLke-iZx0MA0V2IJn9cA8BvoQGr4VBfiu0/edit?usp=sharing">
               <FontAwesome name="file" size="2x" />
             </a>
           </div>
         </nav>
         <div id="hero-block">
-          <img src="/src/images/amber-kim-circle.png" alt="Amber Kim Avatar" id="avatar"/>
+          <img src="/src/images/" alt="Ryan Avatar" id="avatar"/>
           <div className="cta-block">
             <h1>Software Developer</h1>
-            <h3>Background in Web Design + Marketing</h3>
-            <a href="mailto:amber@amberkimdesign.com">
+            <h3>Background in Education and Software Development</h3>
+            <a href="mailto:ryanscottjohnson02@gmail.com">
               <button className="cta-btn">connect</button>
             </a>
           </div>
