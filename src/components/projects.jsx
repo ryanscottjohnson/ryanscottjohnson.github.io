513167f260bd11c0cbce32e59159a88a07499f54
Ryan Scott Johnson <ryanscottjohnson02@gmail.com>
2019-01-08 16:46:57 -0800

    Revert "Updates"

---
+++ src/components/projects.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import './projects.scss';
+
+// import Filter from './filter.jsx';
+import Project from './project.jsx';
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.displayProjects = this.displayProjects.bind(this);
+  }
+
+  displayProjects() {
+    console.log('filter', this.props.projects.filter);
+    let filter = this.props.projects.filter;
+    if (filter === '') {
+      return this.props.projects.projects.map(project => {
+        return <Project key={project.id} id={project.id} details={project}/>
+      });
+    } else {
+      let projectsCopy = this.props.projects.projects.slice();
+      let filteredProjects = projectsCopy.filter(project => {
+        let result = project.tags.indexOf(filter);
+        console.log('result', result);
+        if (result > -1) {
+          return project;
+        }
+      });
+      console.log('filteredProjects', filteredProjects);
+      return filteredProjects.map(project => {
+        return <Project key={project.id} id={project.id} details={project}/>
+      });
+    }
+  }
+
+  render() {
+    return <section>
+      <h2>projects</h2>
+      {/* <Filter /> */}
+      <div className="projects-collection">
+        {this.displayProjects()}
+      </div>
+    </section>
+  }
+}
+
+const mapStateToProps = state => ({
+  projects: state.projects,
+});
+
+const mapDispatchToProps = (dispatch, getState) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Projects);
